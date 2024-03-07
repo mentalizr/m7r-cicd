a5d293c42f4353ab9f83244a692b897340dc0ea7
Arthur Picht <picht@arthurpicht.de>
2024-03-07 21:25:52 +0100

    fix project definition m7r-provisioning

--- src/main/java/org/mentalizr/cicd/project/Provisioning.java
+++ src/main/java/org/mentalizr/cicd/project/Provisioning.java
@@ -1,15 +1,23 @@
 package org.mentalizr.cicd.project;
 
+import de.arthurpicht.utils.core.collection.Sets;
 import org.mentalizr.cicd.build.project.gradle.GradleJarProject;
+import org.mentalizr.cicd.build.project.gradle.GradleToolJarProject;
 import org.mentalizr.commons.paths.host.GitReposDir;
 
 import java.nio.file.Path;
+import java.util.Set;
 
-public class Provisioning extends GradleJarProject {
+public class Provisioning extends GradleToolJarProject {
 
     @Override
     public Path getDir() {
         return GitReposDir.createInstance().asPath().resolve("infra/m7r-provisioning");
+    }
+
+    @Override
+    public Set<String> getDependencies() {
+        return Sets.newHashSet("m7r-commons");
     }
 
 }
