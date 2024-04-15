09dd371ca783775871e2176d5b9a93af87b63a98
Arthur Picht <picht@arthurpicht.de>
2024-04-15 18:54:53 +0200

    dependency update; clean content on command reset

---
+++ src/main/java/org/mentalizr/cicd/build/ContentReset.java
@@ -0,0 +1,23 @@
+package org.mentalizr.cicd.build;
+
+import de.arthurpicht.utils.io.nio2.FileUtils;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+public class ContentReset {
+
+    public static void reset(Path contentDir) throws BuildException {
+        try {
+            List<Path> programDirs = FileUtils.getSubdirectoriesNotEndingWithTilde(contentDir);
+            for (Path programDir : programDirs) {
+                Path htmlDir = programDir.resolve("html");
+                FileUtils.forceDeleteSilently(htmlDir);
+            }
+        } catch (IOException e) {
+            throw new BuildException(e.getMessage(), e);
+        }
+    }
+
+}
--- src/main/java/org/mentalizr/cicd/project/Content.java
+++ src/main/java/org/mentalizr/cicd/project/Content.java
@@ -4,6 +4,7 @@ import de.arthurpicht.taskRunner.task.TaskExecutionException;
 import de.arthurpicht.utils.core.collection.Sets;
 import org.mentalizr.cicd.build.BuildException;
 import org.mentalizr.cicd.build.CommandLineTool;
+import org.mentalizr.cicd.build.ContentReset;
 import org.mentalizr.cicd.projectModel.Project;
 import org.mentalizr.commons.paths.host.GitReposDir;
 
@@ -40,6 +41,11 @@ public class Content extends Project {
 
     @Override
     public void reset() throws TaskExecutionException {
+        try {
+            ContentReset.reset(getDir());
+        } catch (BuildException e) {
+            throw new TaskExecutionException(e.getMessage(), e);
+        }
     }
 
 }
